424b16fa24ec9b1d8e96039e216c29ba2cd125fa
zygon4 <dcharubini@gmail.com>
2020-05-26 22:02:13 -0400

    Formatting

--- memory/src/main/java/com/zygon/htm/memory/core/ActivationMessage.java
+++ memory/src/main/java/com/zygon/htm/memory/core/ActivationMessage.java
@@ -1,19 +1,18 @@
-
 package com.zygon.htm.memory.core;
 
-import com.zygon.htm.core.message.Message;
 import com.zygon.htm.core.Identifier;
+import com.zygon.htm.core.message.Message;
 
 /**
  *
  * @author zygon
  */
 public class ActivationMessage extends Message {
-    
+
     public ActivationMessage(Identifier destination, double value, long timestamp) {
         super(Message.Type.ACTIVATION, destination, value, timestamp);
     }
-    
+
     public ActivationMessage(Identifier destination, double value) {
         this(destination, value, System.currentTimeMillis());
     }
